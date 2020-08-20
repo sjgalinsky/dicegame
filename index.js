7cbf438d27b52dc442140cac1b4d547bbc521187
sjgalinsky <65367275+sjgalinsky@users.noreply.github.com>
2020-08-20 15:22:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var randomNumber1 = Math.floor(Math.random() * 6 + 1);
 var randomNumber2 = Math.floor(Math.random() * 6 + 1);
-document.getElementsByClassName("img1")[0].setAttribute("src" , "images/dice" + randomNumber1 + ".png");
-document.getElementsByClassName("img2")[0].setAttribute("src" , "images/dice" + randomNumber2 + ".png");
+document.getElementsByClassName("img1")[0].setAttribute("src" , "/dice" + randomNumber1 + ".png");
+document.getElementsByClassName("img2")[0].setAttribute("src" , "/dice" + randomNumber2 + ".png");
 
 if (randomNumber1 > randomNumber2) {
   document.querySelector("h1").innerHTML = "Player 1 Wins!"
